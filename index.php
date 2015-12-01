0947829849684840cd0789561be6dbe9b43db9ed
Dave Johnson <dave@silvercg.com>
2015-12-01 16:40:03 -0700

    parsing csv

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+$filePath = 'leads.csv';
 
+function insertCSV($filePath) { #, PDO $con) {
+    $csv = file($filePath);
+    $data = [];
+
+    foreach ($csv as $line) {
+    	$data[] = str_getcsv($line);
+    }
+    print_r($data);
+}
+insertCSV($filePath); #, $con);
 ?>
 
