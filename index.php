0ec8b42fb04dc8312fccf2ec229aa40db67bdc9f
Dave Johnson <dave@silvercg.com>
2015-12-01 17:17:30 -0700

    added comments

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+// Create DB 
 try {
     $con = new PDO("sqlite::memory:");
 } catch(PDOException $e) {
@@ -9,7 +10,8 @@ try {
 $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-$setup_table_query = "CREATE TABLE IF NOT EXSITS Lead (
+// Create table Lead
+$setup_table_query = "CREATE TABLE Lead (
 	LeadID INTEGER PRIMARY KEY AUTOINCREMENT,
 	LeadName VARCHAR(90)
 	)";
@@ -21,16 +23,19 @@ $filePath = 'leads.csv';
 function insertCSV($filePath, PDO $con) {
     $csv = file($filePath);
     $leads = [];
-
+    
+    // Parse csv into array 
     foreach ($csv as $line) {
     	$leads[] = str_getcsv($line);
     }
+    // Input only name because age is not stored?
     foreach ($leads as $lead) {
     	$statement = $con->prepare("INSERT INTO Lead(LeadName) VALUES(?)");
 	$statement->execute([$lead[1]]);
     }
     print_r($leads);
 }
+
 insertCSV($filePath, $con);
 ?>
 
